2f631b875000d0a327d8ad93a1c9e24effe51a3e
Pieter-Jan Steeman <pieterjan.steeman@gmail.com>
2020-03-18 12:49:08 +0100

    Adding subscriber and publisher

---
+++ catkin_ws/src/color_detect/scripts/detector.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import cv2
+import copy
+import numpy as np
+
+class Detector:
+    def __init__(self):
+        self.color_boundries = [
+            	([0, 120, 30], [5, 255, 255], 1),	       # red low
+            	([160, 120, 30], [180, 255, 255], 1),      # red high
+            	([90, 120, 30], [120, 255, 255], 2),       # blue
+            	([50, 120, 30], [80, 255, 255], 3)         # green
+                ]
+        self.cnt_areas = [0, 0, 0, 0]
+
+    def detect_color(self, frame):
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        for (lower, upper, color) in self.color_boundries:
+            # create NumPy arrays from the boundaries
+    		lower = np.array(lower, dtype = "uint8")
+    		upper = np.array(upper, dtype = "uint8")
+
+            # find the colors within the specified boundaries and apply the mask
+    		mask = cv2.inRange(img_roi_hsv, lower, upper)
+
+            _, contours, _ = cv2.findContours(copy.deepcopy(mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    		if len(contours) != 0:
+    			# find the biggest countour (c) by the area
+    			c_max = max(contours, key = cv2.contourArea)
+    			x,y,w,h = cv2.boundingRect(c_max)
+
+    			if(cv2.contourArea(c_max) > self.cnt_areas[color]):
+    				self.cnt_areas[color] = cv2.contourArea(c_max)
+
+        return (cnt_areas.index(max(cnt_areas)))
