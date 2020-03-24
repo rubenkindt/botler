3382034b273a9e5172e53374ded927e28afa4e64
Pieter-Jan Steeman <pieterjan.steeman@gmail.com>
2020-03-24 17:33:38 +0100

    Reworked the code to fix errors, added subscriber and publisher with String data

--- catkin_ws/src/color_detect/scripts/detect_color.py
+++ catkin_ws/src/color_detect/scripts/detect_color.py
@@ -3,6 +3,7 @@ import cv2
 import copy
 import numpy as np
 import rospy
+from std_msgs.msg import String
 
 from detector import Detector
 
@@ -14,31 +15,31 @@ ROI_X_SIZE = 0.5
 ROI_Y_SIZE = 0.5
 
 class Color_detector:
-	__init__(self):
+	def __init__(self):
 		# Subscribe to raspicam feed
-        self.subscriber = rospy.Subscriber('camera/image', cameraFeed, self.callback)
+		self.subscriber = rospy.Subscriber('camera/image', String, self.callback)
 
-        # Create publisher to publish bounding boxes
-        self.color_pub = rospy.Publisher('color_detector/detection', colorDetect, queue_size=1)
+		# Create publisher to publish bounding boxes
+		self.color_pub = rospy.Publisher('color_detector/detection', String, queue_size=1)
 
-        # Detector instance
-        self.detector = Detector()
+		# Detector instance
+		self.detector = Detector()
 
-	def callback(data):
-		cv_image = self.str2cv(data.image)
+	def callback(image):
+		cv_image = self.str2cv(data)
 		color_id = self.detector.detect_color(cv_image)
 		self.color_pub.publish(color_id)
 
 	def str2cv(self, image_string):
-        """ Convert image string to OpenCv image """
-        np_arr = np.fromstring(image_string, np.uint8)
-        return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+		""" Convert image string to OpenCv image """
+		np_arr = np.fromstring(image_string, np.uint8)
+		return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
 if __name__ == '__main__':
 	rospy.init_node('color_detector')
 	color = Color_detector()
 
 	try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
--- catkin_ws/src/color_detect/scripts/detector.py
+++ catkin_ws/src/color_detect/scripts/detector.py
@@ -18,19 +18,19 @@ class Detector:
 
         for (lower, upper, color) in self.color_boundries:
             # create NumPy arrays from the boundaries
-    		lower = np.array(lower, dtype = "uint8")
-    		upper = np.array(upper, dtype = "uint8")
+            lower = np.array(lower, dtype = "uint8")
+            upper = np.array(upper, dtype = "uint8")
 
             # find the colors within the specified boundaries and apply the mask
-    		mask = cv2.inRange(img_roi_hsv, lower, upper)
+            mask = cv2.inRange(img_roi_hsv, lower, upper)
 
             _, contours, _ = cv2.findContours(copy.deepcopy(mask), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    		if len(contours) != 0:
-    			# find the biggest countour (c) by the area
-    			c_max = max(contours, key = cv2.contourArea)
-    			x,y,w,h = cv2.boundingRect(c_max)
+            if len(contours) != 0:
+                # find the biggest countour (c) by the area
+                c_max = max(contours, key = cv2.contourArea)
+                x,y,w,h = cv2.boundingRect(c_max)
 
-    			if(cv2.contourArea(c_max) > self.cnt_areas[color]):
-    				self.cnt_areas[color] = cv2.contourArea(c_max)
+                if(cv2.contourArea(c_max) > self.cnt_areas[color]):
+                    self.cnt_areas[color] = cv2.contourArea(c_max)
 
         return (cnt_areas.index(max(cnt_areas)))
