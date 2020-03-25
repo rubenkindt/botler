79d9984af1e406a16dce5cd33577ab58b11249a4
Ruben Kindt <rubenkindt@live.be>
2020-03-25 12:24:47 +0100

    tempalte matching in ROS pakkage, publisher

---
+++ catkin_ws/src/template_macher/src/template_matching.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import cv2
+import copy
+import numpy as np
+
+class TemplateMatcher:
+    def __init__(self):
+        self.color_boundries = [
+            	([0, 120, 30], [5, 255, 255], 1),	       # red low
+            	([160, 120, 30], [180, 255, 255], 1),      # red high
+            	([90, 120, 30], [120, 255, 255], 2),       # blue
+            	([50, 120, 30], [80, 255, 255], 3)         # green
+                ]
+        self.cnt_areas = [0, 0, 0, 0]
+
+    def templateMatch(	frame = cv2.imread('big_logo.png')):
+    	template = cv2.imread('small_logo.png')
+    	frame=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    	template=cv2.cvtColor(template,cv2.COLOR_BGR2HSV)
+
+    	if (frame is None):
+    		print("image img is none")
+    	if ( template is None):
+    		print("image template is none")
+    	img2 = copy.deepcopy(frame)
+    	h, w = template.shape[0:2]
+
+    	res = cv2.matchTemplate(img2,template,cv2.TM_CCOEFF)
+    	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    	top_left = max_loc
+    	bottom_right = (top_left[0] + w, top_left[1] + h)
+    	cv2.rectangle(img2,top_left, bottom_right, 255, 2)
+    	plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    	plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    	plt.subplot(122),plt.imshow(img2,cmap = 'gray')
+    	plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    	plt.suptitle("cv2.TM_CCOEFF")
+    	plt.show()
+        return (top_left, bottom_right)
