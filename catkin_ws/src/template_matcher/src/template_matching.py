039c1a11f24b7076eb849a75a28e608ff4ad7fa6
Pieter-Jan Steeman <pieterjan.steeman@gmail.com>
2020-04-08 12:35:48 +0200

    preparing implementation and fixing bugs

--- catkin_ws/src/template_matcher/src/template_matching.py
+++ catkin_ws/src/template_matcher/src/template_matching.py
@@ -7,46 +7,37 @@ import numpy as np
 
 class TemplateMatcher:
     def __init__(self):
-        self.color_boundries = [
-            	([0, 120, 30], [5, 255, 255], 1),	       # red low
-            	([160, 120, 30], [180, 255, 255], 1),      # red high
-            	([90, 120, 30], [120, 255, 255], 2),       # blue
-            	([50, 120, 30], [80, 255, 255], 3)         # green
-                ]
-        self.cnt_areas = [0, 0, 0, 0]
+        self.duvel = cv2.imread('pics/small_Duvel.png')
+        self.omer = cv2.imread('pics/small_Omer.png')
+        self.hoe = cv2.imread('pics/small_Hoegaarden.png')
 
-    def templateMatch(self,frame = cv2.imread('big_Hoegaarden.png') ):
-
-        duvel = cv2.imread('small_Duvel.png')
-        omer = cv2.imread('small_Omer.png')
-        hoe = cv2.imread('small_Hoegaarden.png')
-
-        duvel=cv2.cvtColor(duvel,cv2.COLOR_BGR2RGB)
-        omer=cv2.cvtColor(omer,cv2.COLOR_BGR2RGB)
-        hoe=cv2.cvtColor(hoe,cv2.COLOR_BGR2RGB)
-
+        self.duvel=cv2.cvtColor(self.duvel,cv2.COLOR_BGR2RGB)
+        self.omer=cv2.cvtColor(self.omer,cv2.COLOR_BGR2RGB)
+        self.hoe=cv2.cvtColor(self.hoe,cv2.COLOR_BGR2RGB)
+    def templateMatch(self,frame = cv2.imread('pics/big_Hoegaarden.png') ):
         frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
         if (frame is None):
             print("image img is none")
-        if ( duvel is None):
+        if (self.duvel is None):
             print("duvel template is none")
-        if ( omer is None):
+        if (self.omer is None):
             print("omer template is none")
-        if ( hoe is None):
+        if (self.hoe is None):
             print("hoe template is none")
-        templates=(duvel,omer,hoe)
+        templates=(self.duvel,self.omer,self.hoe)
 
         best_photo_nr=0 #see readme.md
         max=0
         teller=0
+
         for template in templates:
             teller += 1
             img2 = copy.deepcopy(frame)
             h, w = template.shape[0:2]
             res = cv2.matchTemplate(img2,template,cv2.TM_CCOEFF)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            print('maxValue',max_val)
+            # print('maxValue',max_val)
             if max < max_val:
                 max = max_val
                 if max_val > 50000000:
@@ -55,17 +46,18 @@ class TemplateMatcher:
         #debug
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
-        cv2.rectangle(img2,top_left, bottom_right, 255, 2)
-        plt.subplot(121),plt.imshow(res,cmap = 'gray')
-        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(img2,cmap = 'gray')
-        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-        plt.suptitle("cv2.TM_CCOEFF")
-        plt.show()
+
+        # cv2.rectangle(img2,top_left, bottom_right, 255, 2)
+        # plt.subplot(121),plt.imshow(res,cmap = 'gray')
+        # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        # plt.subplot(122),plt.imshow(img2,cmap = 'gray')
+        # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        # plt.suptitle("cv2.TM_CCOEFF")
+        # plt.show()
         #debug
 
-        return best_photo_nr
+        return best_photo_nr, top_left, bottom_right
 
-t = TemplateMatcher()
-nr = t.templateMatch()
-print(nr)
+# t = TemplateMatcher()
+# nr = t.templateMatch()
+# print(nr)
---
+++ catkin_ws/src/template_matcher/src/template_matching_node.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import cv2
+import copy
+import numpy as np
+import rospy
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+from template_matching import TemplateMatcher
+
+class LogoFinder:
+	def __init__(self):
+		# Create a bridge to transform topics to CV images and back
+		self.bridge = CvBridge()
+		# Subscribe to camera feed
+		self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.callback)
+
+		# Create publisher to publish detection id
+		self.color_pub = rospy.Publisher('template_matcher/detection_id', String, queue_size = 1)
+
+        # Create publisher to publish bounding boxes
+		self.image_pub = rospy.Publisher('template_matcher/cv_image', Image, queue_size=1)
+
+		# Detector instance
+		self.template_matcher = TemplateMatcher()
+
+	def callback(image):
+		# Try to convert the topic Image to an actual OpenCV image
+		try:
+			cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+		except CvBridgeError as e:
+			print(e)
+
+		# Use template matching to determine the beer type
+		beer_id, tl, br = self.template_matcher.templateMatch(cv_image)
+
+        # Publish the beer_id
+		self.color_pub.publish(str(beer_id))
+
+		if(beer_id != 0):
+			# Draw a bounding box around the logo
+			cv2.rectangle(cv_image, tl, br, (255,255,0), 2)
+
+		# Try to convert the image back to a topic and publish said topic
+		try:
+			self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+		except CvBridgeError as e:
+			print(e)
+
+if __name__ == '__main__':
+	rospy.init_node('template_matcher')
+	color = LogoFinder()
+
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
